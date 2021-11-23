28496b3b6fb3172cd6ef46a05a2b7c1751e7fa01
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-11-22 23:23:16 -0300

    Add requirement 08.AddFavoriteSongs

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
   constructor() {
@@ -8,15 +9,35 @@ export default class MusicCard extends Component {
 
     this.state = {
       loading: false,
+      favoriteSong: false,
     };
+
+    this.onSongs = this.onSongs.bind(this);
+  }
+
+  async onSongs(music) {
+    const { favoriteSong } = this.state;
+
+    if (!favoriteSong) {
+      this.setState({
+        favoriteSong: true,
+        loading: true,
+      });
+    }
+
+    await addSong(music);
+
+    this.setState({
+      loading: false,
+    });
   }
 
   render() {
     const {
-      music: { trackName, previewUrl },
+      music: { trackName, previewUrl, trackId },
     } = this.props;
 
-    const { loading } = this.state;
+    const { loading, favoriteSong } = this.state;
     return loading ? (
       <Loading />
     ) : (
@@ -29,6 +50,16 @@ export default class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="favorite">
+          Favorita
+          <input
+            type="checkbox"
+            id="favorite"
+            data-testid={ `checkbox-music-${trackId}` }
+            checked={ favoriteSong }
+            onChange={ () => this.onSongs() }
+          />
+        </label>
       </div>
     );
   }
@@ -38,4 +69,5 @@ MusicCard.propTypes = {
   music: PropTypes.instanceOf(Object).isRequired,
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.string.isRequired,
 };
